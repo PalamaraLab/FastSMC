9f24bddf61de618145ea04e5ffd33f3804b9cebe
Fergus Cooper <fcooper8472@gmail.com>
2019-07-12 14:47:26 +0100

    #15 Add custom target for regression testing
    Treat the regression test as a regular python script, not run via pytest

--- ASMC_SRC/TESTS/regression_test.py
+++ ASMC_SRC/TESTS/regression_test.py
@@ -1,28 +1,21 @@
 import gzip
 import os
 import subprocess
+import sys
 
 
-def test_regession():
+def test_regession(asmc_exe):
+    """
+    Run the ASMC regression test, which will test the output of an example ASMC run with the cached result in
+    data/regression_test_original.gz.
+
+    :param asmc_exe: path to the ASMC executable
+    """
 
     script_dir = os.path.realpath(os.path.dirname(__file__))
     base_dir = os.path.realpath(os.path.join(script_dir, '..', '..'))
-    bin_dir = os.path.join(base_dir, 'ASMC_SRC', 'BIN')
     old_file = os.path.join(script_dir, 'data', 'regression_test_original.gz')
     assert os.path.isfile(old_file)
-
-    # Find the executable in the BIN dir
-    exe_names = ['ASMC_exe', 'ASMC_exe.exe']
-    asmc_exe = None
-
-    for exe_name in exe_names:
-        exe = os.path.join(bin_dir, exe_name)
-        if os.path.isfile(exe):
-            asmc_exe = exe
-            break
-
-    assert asmc_exe is not None,\
-        "ASMC_exe not found in {}. Ensure ASMC_exe target is built before running this test".format(bin_dir)
 
     # Old file contents are before OxfordRSE involvement in ASMC
     with gzip.open(old_file, 'rt') as gz_f:
@@ -46,8 +39,23 @@ def test_regession():
     with gzip.open(new_file, 'rt') as gz_f:
         new_lines = gz_f.readlines()
 
-    assert len(old_lines) == len(new_lines),\
+    assert len(old_lines) == len(new_lines), \
         "The outputs have different numbers of lines ({} and {})".format(len(old_lines), len(new_lines))
 
     for i, (old, new) in enumerate(zip(old_lines, new_lines)):
         assert old == new, "The outputs first differ at line {}".format(i)
+
+    print('\n' + '#' * 35)
+    print('#      Regression test passed     #')
+    print('# All {} output lines identical #'.format(len(old_lines)))
+    print('#' * 35 + '\n')
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Usage: {} /path/to/ASMC_exe".format(sys.argv[0])
+
+    path_to_asmc = sys.argv[1]
+    assert os.path.isfile(path_to_asmc) and 'ASMC_exe' in path_to_asmc, \
+        "Expected path to ASMC executable, but got {}".format(path_to_asmc)
+
+    test_regession(path_to_asmc)
