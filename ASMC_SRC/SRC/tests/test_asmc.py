455ed6ee034aabc51e42d8584919543de5489efc
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2019-09-25 14:58:02 +0100

    run hmm decodeAll

--- ASMC_SRC/SRC/tests/test_asmc.py
+++ ASMC_SRC/SRC/tests/test_asmc.py
@@ -5,11 +5,16 @@ def test_asmc():
     hapsFileRoot = "FILES/EXAMPLE/exampleFile.n300.array"
     decodingQuantFile = "FILES/DECODING_QUANTITIES" + \
         "/30-100-2000.decodingQuantities.gz"
+    print(decodingQuantFile)
     decodingQuantities = asmc.DecodingQuantities(decodingQuantFile)
     params = asmc.DecodingParams(hapsFileRoot, decodingQuantFile)
     sequenceLength = asmc.Data.countHapLines(hapsFileRoot)
     data = asmc.Data(hapsFileRoot, sequenceLength,
                      decodingQuantities.CSFSSamples,
                      params.foldData, params.usingCSFS)
-    hmm = asmc.HMM(data, decodingQuantities, params, not params.noBatches, False)
+    hmm = asmc.HMM(data, decodingQuantities, params, not params.noBatches, True)
     hmm.decodeAll(params.jobs, params.jobInd)
+
+
+if __name__ == "__main__":
+    test_asmc()
