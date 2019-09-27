b2f889ac010ed7937876830bd3b5766cb514f5c5
Brian Zhang <zhang.brian@gmail.com>
2019-09-27 16:19:21 +0100

    Make sure DecodingParams calls processOptions() on construction
    This seems to fix the unit tests. I've also added a few more
    unit tests relevant to my upcoming work.

--- ASMC_SRC/TESTS/test_asmc.py
+++ ASMC_SRC/TESTS/test_asmc.py
@@ -3,7 +3,8 @@ from asmc import (
     HMM,
     DecodingQuantities,
     DecodingParams,
-    Data
+    Data,
+    makePairObs
 )
 
 
@@ -13,13 +14,13 @@ class TestASMC(unittest.TestCase):
         hapsFileRoot = "FILES/EXAMPLE/exampleFile.n300.array"
         decodingQuantFile = "FILES/DECODING_QUANTITIES" \
             "/30-100-2000.decodingQuantities.gz"
-        sequenceLength = Data.countHapLines(hapsFileRoot)
+        self.sequenceLength = Data.countHapLines(hapsFileRoot)
         params = DecodingParams(hapsFileRoot, decodingQuantFile)
-        decodingQuantities = DecodingQuantities(decodingQuantFile)
-        self.data = Data(hapsFileRoot, sequenceLength,
-                         decodingQuantities.CSFSSamples, params.foldData,
+        self.decodingQuantities = DecodingQuantities(decodingQuantFile)
+        self.data = Data(hapsFileRoot, self.sequenceLength,
+                         self.decodingQuantities.CSFSSamples, params.foldData,
                          params.usingCSFS)
-        self.hmm = HMM(self.data, decodingQuantities, params,
+        self.hmm = HMM(self.data, self.decodingQuantities, params,
                        not params.noBatches, 1)
 
     def test_initialization(self):
@@ -55,6 +56,20 @@ class TestASMC(unittest.TestCase):
         self.assertEqual(self.hmm.getBatchBuffer()[4].jName,
                          self.data.individuals[1].name)
 
+    def test_decode_pair_observation(self):
+        self.assertEqual(len(self.decodingQuantities.discretization),
+                         len(self.decodingQuantities.expectedTimes) + 1)
+        self.assertEqual(self.data.sites, self.sequenceLength)
+
+        for p in [
+            makePairObs(self.data.individuals[0], 1, self.data.individuals[0], 2),
+            makePairObs(self.data.individuals[0], 1, self.data.individuals[1], 1),
+            makePairObs(self.data.individuals[0], 2, self.data.individuals[1], 2)]:
+            d = self.hmm.decode(p)
+            self.assertEqual(len(d), len(self.decodingQuantities.expectedTimes))
+            for i in range(len(d)):
+                self.assertEqual(len(d[i]), self.data.sites)
+
     def test_finish_decoding(self):
         self.assertEqual(len(self.hmm.getBatchBuffer()), 0)
         self.hmm.decodePair(0, 9)
@@ -67,6 +82,30 @@ class TestASMC(unittest.TestCase):
             self.hmm.decodePair(0, i)
         # buffer should be empty now
         self.assertEqual(len(self.hmm.getBatchBuffer()), 0)
+
+class TestASMCDecodingParams(unittest.TestCase):
+    def test_no_compress(self):
+        hapsFileRoot = "FILES/EXAMPLE/exampleFile.n300.array"
+        decodingQuantFile = "FILES/DECODING_QUANTITIES" \
+            "/30-100-2000.decodingQuantities.gz"
+        sequenceLength = Data.countHapLines(hapsFileRoot)
+        params = DecodingParams(hapsFileRoot, decodingQuantFile, compress=True,
+            skipCSFSdistance=float('nan'))
+
+        self.assertEqual(params.compress, True)
+        self.assertEqual(params.skipCSFSdistance, float('inf'))
+
+        decodingQuantities = DecodingQuantities(decodingQuantFile)
+        data = Data(hapsFileRoot, sequenceLength,
+                    decodingQuantities.CSFSSamples, params.foldData,
+                    params.usingCSFS)
+        hmm = HMM(data, decodingQuantities, params, not params.noBatches, 1)
+
+        p = makePairObs(data.individuals[0], 1, data.individuals[0], 2)
+        d = hmm.decode(p)
+        self.assertEqual(len(d), len(decodingQuantities.expectedTimes))
+        for i in range(len(d)):
+            self.assertEqual(len(d[i]), data.sites)
 
 
 if __name__ == "__main__":
