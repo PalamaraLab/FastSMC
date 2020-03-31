c851a5cbcd239f86e681bbbab5c1b878859fd4f2
Fergus Cooper <fcooper8472@gmail.com>
2020-03-31 23:48:01 +0100

    #34 Add regression test for FastSMC without GERMLINE

--- ASMC_SRC/TESTS/test_fastsmc_regression.cpp
+++ ASMC_SRC/TESTS/test_fastsmc_regression.cpp
@@ -29,7 +29,7 @@
 
 
 
-TEST_CASE("test FastSMC HMM with regression test", "[FastSMC_regression]")
+TEST_CASE("test FastSMC + GERMLINE regression test", "[FastSMC_regression]")
 {
   DecodingParams params;
   params.decodingQuantFile = ASMC_FILE_DIR
@@ -90,6 +90,85 @@ TEST_CASE("test FastSMC HMM with regression test", "[FastSMC_regression]")
     {
       FileUtils::AutoGzIfstream fin_generated;
       fin_generated.openOrExit(params.outFileRoot + ".1.1.FastSMC.ibd.gz");
+      for (std::string f_line; getline(fin_generated, f_line);) {
+        generatedLines.emplace_back(f_line);
+      }
+      fin_generated.close();
+    }
+
+    REQUIRE(regressionLines.size() == expectedNumLines);
+    REQUIRE(regressionLines.size() == generatedLines.size());
+
+    for (auto lineNum = 0ul; lineNum < regressionLines.size(); ++lineNum) {
+      REQUIRE(regressionLines.at(lineNum) == generatedLines.at(lineNum));
+    }
+  }
+}
+
+
+TEST_CASE("test FastSMC without GERMLINE regression test", "[FastSMC_regression]")
+{
+  DecodingParams params;
+  params.decodingQuantFile = ASMC_FILE_DIR
+                             "/FASTSMC_EXAMPLE/out.25.n300.chr2.len30.dens1.disc10-20-2000.demoCEU.mapnorm.array.decodingQuantities.gz";
+  params.inFileRoot =
+      ASMC_FILE_DIR "/FASTSMC_EXAMPLE/out.25.n300.chr2.len30.dens1.disc10-20-2000.demoCEU.mapnorm.array";
+  params.outFileRoot = "/tmp/FastSMCresults";
+  params.decodingModeString = "array";
+  params.decodingMode = DecodingMode::arrayFolded;
+  params.foldData = true;
+  params.usingCSFS = true;
+  params.batchSize = 32;
+  params.recallThreshold = 3;
+  params.min_m = 1.5;
+  params.GERMLINE = false;
+  params.FastSMC = true;
+  params.BIN_OUT = false;
+  params.time = 50;
+  params.noConditionalAgeEstimates = true;
+  params.doPerPairMAP = true;
+  params.doPerPairPosteriorMean = true;
+  params.jobInd = 7;
+  params.jobs = 9;
+
+  // read decoding quantities from file
+  DecodingQuantities decodingQuantities(params.decodingQuantFile.c_str());
+  const auto sequenceLength = Data::countHapLines(params.inFileRoot);
+
+  const bool useKnownSeed = true;
+  Data data(params.inFileRoot, sequenceLength, decodingQuantities.CSFSSamples, params.foldData, params.usingCSFS,
+            params.jobInd, params.jobs, useKnownSeed);
+
+  HMM hmm(data, decodingQuantities, params, !params.noBatches);
+
+  const int WORD_SIZE = 64;
+  const int CONST_READ_AHEAD = 10;
+  const bool PAR_HAPLOID = true;
+  ASMC::FastSMC fastSMC(WORD_SIZE, CONST_READ_AHEAD, PAR_HAPLOID);
+  fastSMC.run(params, data, hmm);
+
+  SECTION("regression test")
+  {
+    const auto expectedNumLines = 3056ul;
+
+    // Read lines from existing regression test output into a vector of strings
+    std::vector<std::string> regressionLines;
+    regressionLines.reserve(expectedNumLines);
+    {
+      FileUtils::AutoGzIfstream fin_regression;
+      fin_regression.openOrExit(ASMC_FILE_DIR "/FASTSMC_EXAMPLE/regression_output_no_germline.ibd.gz");
+      for (std::string f_line; getline(fin_regression, f_line);) {
+        regressionLines.emplace_back(f_line);
+      }
+      fin_regression.close();
+    }
+
+    // Read lines from generated test output into a vector of strings
+    std::vector<std::string> generatedLines;
+    generatedLines.reserve(expectedNumLines);
+    {
+      FileUtils::AutoGzIfstream fin_generated;
+      fin_generated.openOrExit(params.outFileRoot + ".7.9.FastSMC.ibd.gz");
       for (std::string f_line; getline(fin_generated, f_line);) {
         generatedLines.emplace_back(f_line);
       }
--- ASMC_SRC/TESTS/test_regression.py
+++ ASMC_SRC/TESTS/test_regression.py
@@ -81,3 +81,52 @@ class TestFastSMCRegression(unittest.TestCase):
 
         self.assertEqual(original_text.shape, generated_text.shape)
         self.assertEqual(np.allclose(original_text, generated_text), True)
+
+
+class TestFastSMCRegressionWithoutGermline(unittest.TestCase):
+
+    def setUp(self):
+        self.file_dir = os.path.join(os.getcwd(), 'FILES', 'FASTSMC_EXAMPLE')
+        self.name_prefix = 'out.25.n300.chr2.len30.dens1.disc10-20-2000.demoCEU.mapnorm.array'
+
+        # Create decoding params object with required options
+        self.params = DecodingParams()
+        self.params.decodingQuantFile = os.path.join(self.file_dir, '{}.decodingQuantities.gz'.format(self.name_prefix))
+        self.params.inFileRoot = os.path.join(self.file_dir, self.name_prefix)
+        self.params.outFileRoot = os.path.join('/tmp/FastSMCresults')
+        self.params.decodingModeString = 'array'
+        self.params.decodingMode = DecodingMode.arrayFolded
+        self.params.foldData = True
+        self.params.usingCSFS = True
+        self.params.batchSize = 32
+        self.params.recallThreshold = 3
+        self.params.min_m = 1.5
+        self.params.GERMLINE = False
+        self.params.FastSMC = True
+        self.params.BIN_OUT = False
+        self.params.time = 50
+        self.params.noConditionalAgeEstimates = True
+        self.params.doPerPairMAP = True
+        self.params.doPerPairPosteriorMean = True
+        self.params.jobInd = 7
+        self.params.jobs = 9
+
+        decoding_quantities = DecodingQuantities(self.params.decodingQuantFile)
+        sequence_length = Data.countHapLines(self.params.inFileRoot)
+
+        use_known_seed = True
+        data = Data(self.params.inFileRoot, sequence_length, decoding_quantities.CSFSSamples,
+                    self.params.foldData, self.params.usingCSFS, self.params.jobInd, self.params.jobs, use_known_seed)
+
+        hmm = HMM(data, decoding_quantities, self.params, not self.params.noBatches, 1)
+
+        fast_smc = FastSMC(hashingWordSize=64, constReadAhead=10, haploid=True)
+        fast_smc.run(self.params, data, hmm)
+
+    def test_regression(self):
+        original_text = np.loadtxt(os.path.join(self.file_dir, 'regression_output_no_germline.ibd.gz'),
+                                   usecols=(7, 8, 9, 10, 11))
+        generated_text = np.loadtxt(self.params.outFileRoot + ".7.9.FastSMC.ibd.gz", usecols=(7, 8, 9, 10, 11))
+
+        self.assertEqual(original_text.shape, generated_text.shape)
+        self.assertEqual(np.allclose(original_text, generated_text), True)
