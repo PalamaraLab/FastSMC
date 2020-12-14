a2df4fa9b59d9e201e2ad68f52c7251977539ed4
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-12-14 19:47:08 +0000

    python: Bump version to .post1 for macOS rebuild

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='asmc',
-    version='0.1',
+    version='0.1.post1',
     author='Pier Palamara',
     author_email='palamara@stats.ox.ac.uk',
     description='ASMC is a method to efficiently estimate pairwise coalescence time along the genome',
